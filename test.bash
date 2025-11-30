78859c6bde6a7aef5ad7230f9ae0d863ea7b5a1f
Ro Yuken <yuken.lu3@gmail.com>
2025-11-30 16:30:59 +0900

    Improve the test

--- test.bash
+++ test.bash
@@ -1,4 +1,18 @@
-#!/bin/bash
+#!/bin/bash -xv
+# SPDX-FileCopyrightText: 2025 Yuken Ro
+# SPDX-License-Identifier: GPL-3.0-only
 
-seq 5 | ./plus
+ng () {
+	echo NG at line $1
+	res=1
+}
+
+res=0
+
+out=$(seq 5 | ./plus)
+
+[ "${out}" = 15 ] || ng $LINRNO
+
+[ "$res" = 0 ] && echo OK
+exit $res
 
